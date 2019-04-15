62a04f3350d2a8081103ff73ddc13ebdc4c3ed03
Aidan Lytle <aflytle@uncg.edu>
2019-04-15 15:37:11 -0400

    Yay

--- simple_particle.C
+++ simple_particle.C
@@ -14,6 +14,7 @@
 #include "TProfile.h"
 #include "TCanvas.h"
 #include "TFile.h"
+#include "TComplex.h"
 
 #include "Pythia8/Pythia.h"
 
@@ -31,6 +32,17 @@ int main()
   pythia.readString("PhaseSpace:pTHatMin = 20.");
   pythia.init();
 
+  // Stuff for cumulants greater than 2
+  static const int maxCorrelator = 12; // Somewhat abusing the setup as it is...
+  static const int maxHarmonic = 10; // Need to assess on case-by-case basis, but this gets you v2{8} and v3{6}
+  static const int maxPower = 9;
+  TComplex Qvector[maxHarmonic][maxPower]; // All needed Q-vector components
+  TComplex Q(int, int);
+  TComplex Recursion(int, int*);
+  TComplex Recursion(int, int*, int, int);
+
+
+
   // --- histograms
   TH1D* hmult = new TH1D("hmult","charged multiplicity", 100, -0.5, 499.5);
   TH1D* hmult_selected = new TH1D("hmult_selected","charged multiplicity for selected particles", 100, -0.5, 499.5);
@@ -47,9 +59,74 @@ int main()
   // TH1D* hPhi_vec = new TH1D("hPhi w/counter", "phi distribution", 100, -pi, pi);
   TH1D* heta_vec = new TH1D("heta_vec", "eta distribution", 100, -3, 3);
   TH1D* hPhi_vec = new TH1D("hPhi_vec", "phi distribution", 100, -pi, pi);
+
   // Begin event loop. Generate event. Skip if error. List first one.
+  // Particle correlation as a function of charged multiplicity.
 
   TProfile* tp1f_c22mult = new TProfile("tp1f_c22mult","c22 vs mult",100,-0.5,499.5,-1e10,1e10);
+  TProfile* tp1f_c24mult = new TProfile("tp1f_c24mult","c24 vs mult",100,-0.5,499.5,-1e10,1e10);
+  TProfile* tp1f_c26mult = new TProfile("tp1f_c26mult","c26 vs mult",100,-0.5,499.5,-1e10,1e10);
+  TProfile* tp1f_c28mult = new TProfile("tp1f_c28mult","c28 vs mult",100,-0.5,499.5,-1e10,1e10);
+
+  TProfile* nfvtxt_recursion[2][maxCorrelator];
+  for ( int cs = 0; cs < 2; ++cs )
+    {
+      for(int c = 0; c < maxCorrelator; ++c )
+        {
+          nfvtxt_recursion[cs][c] = new TProfile(Form(“nfvtxt_recursion_%d_%d”,cs,c),“”,700,-0.5,699.5,-1.1,1.1);
+        }
+    }
+
+  ////
+  //Recursion Function from BoulderCumulantsFunction.C
+  ////
+
+  // --- from generic forumulas ----------------------------------------------------
+  TComplex BoulderCumulants::Recursion(int n, int* harmonic)
+  {
+    return Recursion(n,harmonic,1,0); // 1 and 0 are defaults from above
+  }
+
+  TComplex BoulderCumulants::Recursion(int n, int* harmonic, int mult, int skip)
+  {
+    // Calculate multi-particle correlators by using recursion (an improved faster version) originally developed by
+    // Kristjan Gulbrandsen (gulbrand@nbi.dk).
+
+    int nm1 = n-1;
+    TComplex c(Q(harmonic[nm1], mult));
+    if (nm1 == 0) return c;
+    c *= Recursion(nm1, harmonic);
+    if (nm1 == skip) return c;
+
+    int multp1 = mult+1;
+    int nm2 = n-2;
+    int counter1 = 0;
+    int hhold = harmonic[counter1];
+    harmonic[counter1] = harmonic[nm2];
+    harmonic[nm2] = hhold + harmonic[nm1];
+    TComplex c2(Recursion(nm1, harmonic, multp1, nm2));
+    int counter2 = n-3;
+    while (counter2 >= skip) {
+      harmonic[nm2] = harmonic[counter1];
+      harmonic[counter1] = hhold;
+      ++counter1;
+      hhold = harmonic[counter1];
+      harmonic[counter1] = harmonic[nm2];
+      harmonic[nm2] = hhold + harmonic[nm1];
+      c2 += Recursion(nm1, harmonic, multp1, counter2);
+      --counter2;
+    }
+    harmonic[nm2] = harmonic[counter1];
+    harmonic[counter1] = hhold;
+
+    if (mult == 1) return c-c2;
+    return c-double(mult)*c2;
+
+  }
+
+  ////
+  //Main event loop
+  ////
 
   for (int iEvent = 0; iEvent < 10000; ++iEvent)
     {
@@ -104,7 +181,48 @@ int main()
 
         } // end loop over particles
 
-      // calculate the cumulant
+      ////Defining usage and objects necessary for Recursion and 
+      ////higher cumulants after particle loop, but over 
+      ////event loop
+
+      int harmonics_Two_Num[2] = {2,-2}; // 2, -2
+      int harmonics_Two_Den[2] = {0,0}; // recursion gives right combinatorics
+      TComplex twoRecursion = Recursion(2,harmonics_Two_Num)/Recursion(2,harmonics_Two_Den).Re();
+      double spwTwoRecursion = Recursion(2,harmonics_Two_Den).Re();
+      double wTwoRecursion = 1.0;
+      nfvtxt_recursion[0][0]->Fill(nfvtxt,twoRecursion.Re(),wTwoRecursion); // <<cos(h1*phi1+h2*phi2)>>
+      nfvtxt_recursion[1][0]->Fill(nfvtxt,twoRecursion.Im(),wTwoRecursion); // <<sin(h1*phi1+h2*phi2)>>
+      //  4-p correlations:
+      //cout<<” => Calculating 4-p correlations (using recursion)...       \r”<<flush;
+      int harmonics_Four_Num[4] = {2,2,-2,-2};
+      int harmonics_Four_Den[4] = {0,0,0,0}; // recursion gives right combinatorics
+      TComplex fourRecursion = Recursion(4,harmonics_Four_Num)/Recursion(4,harmonics_Four_Den).Re();
+      double spwFourRecursion = Recursion(4,harmonics_Four_Den).Re();
+      double wFourRecursion = 1.0;
+      nfvtxt_recursion[0][2]->Fill(nfvtxt,fourRecursion.Re(),wFourRecursion); // <<cos(h1*phi1+h2*phi2+h3*phi3+h4*phi4)>>
+      nfvtxt_recursion[1][2]->Fill(nfvtxt,fourRecursion.Im(),wFourRecursion); // <<sin(h1*phi1+h2*phi2+h3*phi3+h4*phi4)>>
+      //  6-p correlations:
+      //cout<<” => Calculating 6-p correlations (using recursion)...       \r”<<flush;
+      int harmonics_Six_Num[6] = {2,2,2,-2,-2,-2};
+      int harmonics_Six_Den[6] = {0,0,0,0,0,0};
+      TComplex sixRecursion = Recursion(6,harmonics_Six_Num)/Recursion(6,harmonics_Six_Den).Re();
+      double spwSixRecursion = Recursion(6,harmonics_Six_Den).Re();
+      double wSixRecursion = 1.0;
+      nfvtxt_recursion[0][4]->Fill(nfvtxt,sixRecursion.Re(),wSixRecursion); // <<cos(h1*phi1+h2*phi2+h3*phi3+h4*phi4+h5*phi5+h6*phi6)>>
+      nfvtxt_recursion[1][4]->Fill(nfvtxt,sixRecursion.Im(),wSixRecursion); // <<sin(h1*phi1+h2*phi2+h3*phi3+h4*phi4+h5*phi5+h6*phi6)>>
+      //  8-p correlations:
+      //cout<<” => Calculating 8-p correlations (using recursion)...       \r”<<flush;
+      int harmonics_Eight_Num[8] = {2,2,2,2,-2,-2,-2,-2};
+      int harmonics_Eight_Den[8] = {0,0,0,0,0,0,0,0};
+      TComplex eightRecursion = Recursion(8,harmonics_Eight_Num)/Recursion(8,harmonics_Eight_Den).Re();
+      double spwEightRecursion = Recursion(8,harmonics_Eight_Den).Re();
+      double wEightRecursion = 1.0;
+      nfvtxt_recursion[0][6]->Fill(nfvtxt,eightRecursion.Re(),wEightRecursion);
+      nfvtxt_recursion[1][6]->Fill(nfvtxt,eightRecursion.Im(),wEightRecursion);
+      ////End of ripped codes from 'Boulder' codes
+
+
+      // calculate the 2 cumulant
       double c22 = ((Q2x * Q2y) - mult)/(mult*(mult-1));
       // fill TProfile to compute average cumulant vs multiplicity
       tp1f_c22mult->Fill(mult,c22);
