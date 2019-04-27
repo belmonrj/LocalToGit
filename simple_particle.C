d46c98fea00cc91548921222f8916666a93f5ffe
Aidan Lytle <aflytle@uncg.edu>
2019-04-27 16:29:09 -0400

    Have your look, you

--- simple_particle.C
+++ simple_particle.C
@@ -88,6 +88,15 @@ int main()
 
   for (int iEvent = 0; iEvent < 1000; ++iEvent)
     {
+      // --- for the generic formulas ---------
+      for(int h=0;h<maxHarmonic;h++)
+        {
+          for(int w=0;w<maxPower;w++)
+            {
+              Qvector[h][w] = TComplex(0.,0.);
+            } //  for(int p=0;p<maxPower;p++)
+        } // for(int h=0;h<maxHarmonic;h++)
+      // --------------------------------------
 
       if (!pythia.next()) continue;
       // Find number of all final charged particles and fill histogram.
@@ -138,14 +147,14 @@ int main()
           // Qy
           
 
-      for(int h=0;h<maxHarmonic;h++)
-        {
-          for(int w=0;w<maxPower;w++)
+          for(int h=0;h<maxHarmonic;h++)
             {
-              //if(bUseWeights){wPhiToPowerP = pow(wPhi,p);} // no weights for us...
-              Qvector[h][w] += TComplex(cos(h*p.phi()),sin(h*p.phi()));
-            } //  for(int w=0;w<maxPower;w++)
-        } // for(int h=0;h<maxHarmonic;h++)
+              for(int w=0;w<maxPower;w++)
+                {
+                  //if(bUseWeights){wPhiToPowerP = pow(wPhi,p);} // no weights for us...
+                  Qvector[h][w] += TComplex(cos(h*p.phi()),sin(h*p.phi()));
+                } //  for(int w=0;w<maxPower;w++)
+            } // for(int h=0;h<maxHarmonic;h++)
 
         } // end loop over particles
 
