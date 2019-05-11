5849810ebdaaf0818626fceed5f2d11169015053
Aidan Lytle <aflytle@uncg.edu>
2019-05-11 12:40:30 -0400

    I have added the math operations

--- CumulantFromCK.C
+++ CumulantFromCK.C
@@ -11,14 +11,15 @@ void CumulantFromCK()
   TProfile* hm_2 = (TProfile*)file->Get("hmult_recursion_0_2");
   cout << hm_2 << endl;
   TH1D* hm_2_new = hm_2->ProjectionX("hm_2_new", "B");
+  hm_2_new = (hm_2_new) - (2*(hm_0_new**2))
 
-  TProfile* hm_4 = (TProfile*)file->Get("hmult_recursion_0_4");
-  cout << hm_4 << endl;
-  TH1D* hm_4_new = hm_4->ProjectionX("hm_4_new", "B");
+    //  TProfile* hm_4 = (TProfile*)file->Get("hmult_recursion_0_4");
+    //  cout << hm_4 << endl;
+    //  TH1D* hm_4_new = hm_4->ProjectionX("hm_4_new", "B");
 
-  TProfile* hm_6 = (TProfile*)file->Get("hmult_recursion_0_6");
-  cout << hm_6 << endl;
-  TH1D* hm_6_new = hm_6->ProjectionX("hm_6_new", "B");
+    //  TProfile* hm_6 = (TProfile*)file->Get("hmult_recursion_0_6");
+    //  cout << hm_6 << endl;
+    //  TH1D* hm_6_new = hm_6->ProjectionX("hm_6_new", "B");
 
   return;
 
