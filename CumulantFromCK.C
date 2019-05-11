587e6c03956bf596652e1f7d49800650e97240bf
Aidan Lytle <aflytle@uncg.edu>
2019-05-11 12:48:56 -0400

    Here it is with the fix

--- CumulantFromCK.C
+++ CumulantFromCK.C
@@ -7,12 +7,16 @@ void CumulantFromCK()
   TProfile* hm_0 = (TProfile*)file->Get("hmult_recursion_0_0");
   cout << hm_0 << endl;
   TH1D* hm_0_new = hm_0->ProjectionX("hm_0_new", "B");
+  TH1D* H2 = hm_0_new; // Copy for math operations
 
   TProfile* hm_2 = (TProfile*)file->Get("hmult_recursion_0_2");
   cout << hm_2 << endl;
   TH1D* hm_2_new = hm_2->ProjectionX("hm_2_new", "B");
-  hm_2_new = (hm_2_new) - (2*(hm_0_new**2))
-
+  TH1D* H4 = hm_2_new; // Copy for math operations
+  H2->Multiply(H2);
+  H2->Scale(2.0);
+  H4->Add(H2,-1.0);
+  
     //  TProfile* hm_4 = (TProfile*)file->Get("hmult_recursion_0_4");
     //  cout << hm_4 << endl;
     //  TH1D* hm_4_new = hm_4->ProjectionX("hm_4_new", "B");
