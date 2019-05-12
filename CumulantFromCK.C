8b335279b12ee3dba5703bcd27e42da30e5cdf8e
Aidan Lytle <aflytle@uncg.edu>
2019-05-11 21:30:56 -0400

    Fixed the typecasting on the new TH1D objects

--- CumulantFromCK.C
+++ CumulantFromCK.C
@@ -6,31 +6,31 @@ void CumulantFromCK()
   //Definitions of <<2>>
   TProfile* hm_0 = (TProfile*)file->Get("hmult_recursion_0_0");
   cout << hm_0 << endl;
-  TH1D* hm_2_new = hm_0->ProjectionX("hm_0_new", "B");
-  TH1D* H2_0 = hm_2_new->Clone("H2_0"); // Copy for math operations for <<4>>
-  TH1D* H2_1 = hm_2_new->Clone("H2_1"); // Copy for math operations for <<6>>
-  TH1D* H2_2 = hm_2_new->Clone("H2_2"); // Copy for math operations for <<8>>
+  TH1D* hm_2_new = (TH1D*)hm_0->ProjectionX("hm_0_new", "B");
+  TH1D* H2_0 = (TH1D*)hm_2_new->Clone("H2_0"); // Copy for math operations for <<4>>
+  TH1D* H2_1 = (TH1D*)hm_2_new->Clone("H2_1"); // Copy for math operations for <<6>>
+  TH1D* H2_2 = (TH1D*)hm_2_new->Clone("H2_2"); // Copy for math operations for <<8>>
 
 
   //Definitions of <<4>>
   TProfile* hm_2 = (TProfile*)file->Get("hmult_recursion_0_2");
   cout << hm_2 << endl;
-  TH1D* hm_4_new = hm_2->ProjectionX("hm_2_new", "B");
-  TH1D* H4_0 = hm_4_new->Clone("H4_0"); // Copy for math operations for <<6>>
-  TH1D* H4_1 = hm_4_new->Clone("H4_1"); // Copy for math operations for <<8>>
-  TH1D* H4_2 = hm_4_new->Clone("H4_2"); // Copy for math operations for <<8>> #2
+  TH1D* hm_4_new = (TH1D*)hm_2->ProjectionX("hm_2_new", "B");
+  TH1D* H4_0 = (TH1D*)hm_4_new->Clone("H4_0"); // Copy for math operations for <<6>>
+  TH1D* H4_1 = (TH1D*)hm_4_new->Clone("H4_1"); // Copy for math operations for <<8>>
+  TH1D* H4_2 = (TH1D*)hm_4_new->Clone("H4_2"); // Copy for math operations for <<8>> #2
   //Math on <<4>>
   H2_0->Multiply(H2_0); // H2 -> (H2)^2
   H2_0->Scale(2.0); // H2 -> 2(H2)^2
-  hm_2_new->Add(H2_0,-1.0); // H4 -> H4 - 2(H2^2)
+  hm_4_new->Add(H2_0,-1.0); // H4 -> H4 - 2(H2^2)
 
 
 
   //Definitions of <<6>>
   TProfile* hm_4 = (TProfile*)file->Get("hmult_recursion_0_4");
   cout << hm_4 << endl;
-  TH1D* hm_6_new = hm_4->ProjectionX("hm_4_new", "B");
-  TH1D* H6_0 = hm_6_new->Clone("H6_0"); // Copy for math operations for <<6>>
+  TH1D* hm_6_new = (TH1D*)hm_4->ProjectionX("hm_4_new", "B");
+  TH1D* H6_0 = (TH1D*)hm_6_new->Clone("H6_0"); // Copy for math operations for <<6>>
 
 
 
@@ -50,7 +50,7 @@ void CumulantFromCK()
   //Definitions of <<8>>
   TProfile* hm_6 = (TProfile*)file->Get("hmult_recursion_0_6");
   cout << hm_6 << endl;
-  TH1D* hm_8_new = hm_6->ProjectionX("hm_6_new", "B");
+  TH1D* hm_8_new = (TH1D*)hm_6->ProjectionX("hm_6_new", "B");
 
   //Math on <<8>>
   H6_0->Multiply(H2_2); // H6 -> (H6)(H2)
