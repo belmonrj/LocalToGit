3de5df2c492c944acae1e430fb4c0f2e0582faf1
Aidan Lytle <aflytle@uncg.edu>
2019-05-20 20:56:59 -0400

    Added the phi_a-phi_b term

--- RandomPhi.C
+++ RandomPhi.C
@@ -13,13 +13,16 @@ void RandomPhi()
   int events = 20; //put events number here
   double j[events];
   double k[events];
+  double l[events];
   int i = 0;
   for(i = 0; i <= events; ++i)
     {
     double phi = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/pi));
     j[i] = phi;
     k[i] = phi + pi;
+    l[i] = j[i]-k[i];
     }
+
 
   return;
 
