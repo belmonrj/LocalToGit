6fade5b3d8df87aa895d77213329cd351f9dc0c2
Aidan Lytle <aflytle@uncg.edu>
2019-05-20 20:51:45 -0400

    Here is the basis for the random phi_a and phi_b generator

---
+++ RandomPhi.C
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+void RandomPhi()
+{
+  srand (static_cast <unsigned> (time(0)));
+  double pi = 3.1415;
+  int events = 20; //put events number here
+  double j[events];
+  double k[events];
+  int i = 0;
+  for(i = 0; i <= events; ++i)
+    {
+    double phi = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/pi));
+    j[i] = phi;
+    k[i] = phi + pi;
+    }
+
+  return;
+
+}
+
