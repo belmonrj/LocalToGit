1f873cb70d3f6e83b89c177ac67582bc7927af23
Aidan Lytle <aflytle@uncg.edu>
2019-05-23 08:51:07 -0400

    Added axis labelling edits.

--- RandomPhi.C
+++ RandomPhi.C
@@ -9,20 +9,20 @@ using namespace std;
 void RandomPhi()
 {
   srand (static_cast <unsigned> (time(0)));
-  double pi = 3.1415;
+  double pi = 3.1415926;
 
 
   // Objects for printing/filling
-  TH1D* phi_for_monty = new TH1D("phi_for_monty", "Test Phi Print", 100, 0, pi);
+  TH1D* phi_for_monty = new TH1D("phi_for_monty", "Test Phi Print", 100, 0, pi); // can label axes with "Title; XAxisTitle; YAxisTitle"
   TCanvas* c1 = new TCanvas("c1", "samples",200, 10 , 600, 400);
 
 
   //important typecasting
   int pairs = 50; //put number of pairs per event here
-
   int events = 1000;
 
-  //loop of events and
+
+  //loop of events and pairs
   for(int m = 0; m <= events; ++m){
 
     for(int i = 0; i <= pairs; ++i)
@@ -33,6 +33,8 @@ void RandomPhi()
         phi_for_monty->Fill(j,1.0);
       }
   }
+  phi_for_monty->GetXaxis()->SetTitle("XAxis");
+  phi_for_monty->GetYaxis()->SetTitle("Yaxis");
     phi_for_monty->Draw();
     c1->Print("phiForMrBurns.png");
 
