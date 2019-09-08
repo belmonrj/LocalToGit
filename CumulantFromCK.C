6ef4a63126948cb7d879b16fad587928d3157409
Aidan Lytle <aflytle@uncg.edu>
2019-09-08 19:48:58 -0400

    Fixed

--- CumulantFromCK.C
+++ CumulantFromCK.C
@@ -207,11 +207,17 @@ void CumulantFromCK()
 
 
 
-  TF1* N2 = new TF1("N2","[0]*1/(x-1)",2,500);
+  //TF1* N2 = new TF1("N2","[0]*1/(x-1)",2,500);
+  //TF1* N2 = new TF1("N2","[0]*1/pow(x,[1])",2,500);
+  //TF1* N2 = new TF1("N2","[0}*1/pow(x-1,[0]",2,500);
+  TF1* N2 = new TF1("N2","[0]*1/pow(x-[1],[2])",2,500);
+  N2->SetParLimits(1,0,1);
   TF1* N4 = new TF1("N4","[0]*6/((x-1)*(x-2)*(x-3))",4,500);
+  N4->SetParLimits(1,0,1);
   TF1* N6 = new TF1("N6","[0]*120/((x-1)*(x-2)*(x-3)*(x-4)*(x-5)*(x-6))",6,500);
-  TF1* N8 = new TF1("N8","[0]*5040/((x-1)*(x-2)*(x-3)*(x-4)*(x-5)*(x-6)*(x-7)*(x-8)",8,500);
-
+  N6->SetParLimits(1,0,1);
+  TF1* N8 = new TF1("N8","[0]*5040/((x-1)*(x-2)*(x-3)*(x-4)*(x-5)*(x-6)*(x-7)*(x-8))",8,500);
+  N8->SetParLimits(1,0,1);
   // Sample class for TF1 --> TF1* function = new TF1(“function”,“1/(x-1)“,2,500)
 
   cout << hm_2_new << endl;
